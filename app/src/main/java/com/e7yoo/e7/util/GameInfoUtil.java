c36ebbd132e99014a64a02ee754657699a30ac41
Thinkin <thinkin.liu@gmail.com>
2017-09-29 09:49:33 +0800

    h5游戏增加分享title和content

--- app/src/main/java/com/e7yoo/e7/util/GameInfoUtil.java
+++ app/src/main/java/com/e7yoo/e7/util/GameInfoUtil.java
@@ -17,13 +17,28 @@ import java.util.ArrayList;
 public class GameInfoUtil {
     public static ArrayList<GameInfo> getGameInfos(Context context) {
         ArrayList<GameInfo> gameInfos = new ArrayList<>();
-        gameInfos.add(new GameInfo(context.getString(R.string.gamelist_game2048), context.getString(R.string.gamelist_game2048_content), 0, null, Game2048Activity.class.getSimpleName()));
-        gameInfos.add(new GameInfo(context.getString(R.string.gamelist_killbird), context.getString(R.string.gamelist_killbird_content), 0, null, KillBirdActivity.class.getSimpleName()));
-        gameInfos.add(new GameInfo(context.getString(R.string.gamelist_plane), context.getString(R.string.gamelist_plane_content), 0, null, PlaneMainActivity.class.getSimpleName()));
-        gameInfos.add(new GameInfo(context.getString(R.string.game_h5_zhaoyaojing), context.getString(R.string.game_h5_zhaoyaojing_content), 1, "http://h.4399.com/play/161856.htm", null));
-        gameInfos.add(new GameInfo(context.getString(R.string.game_h5_zaqiche), context.getString(R.string.game_h5_zaqiche_content), 1, "http://h.4399.com/play/159875.htm", null));
-        gameInfos.add(new GameInfo(context.getString(R.string.game_h5_wucaiguodong), context.getString(R.string.game_h5_wucaiguodong_content), 2, "http://h.4399.com/play/191530.htm", null));
-        gameInfos.add(new GameInfo(context.getString(R.string.game_h5_guoshanche), context.getString(R.string.game_h5_guoshanche_content), 2, "http://h.4399.com/play/158682.htm", null));
+        GameInfo gameInfo = new GameInfo(context.getString(R.string.gamelist_game2048), context.getString(R.string.gamelist_game2048_content), 0, null, Game2048Activity.class.getSimpleName());
+        gameInfos.add(gameInfo);
+        gameInfo = new GameInfo(context.getString(R.string.gamelist_killbird), context.getString(R.string.gamelist_killbird_content), 0, null, KillBirdActivity.class.getSimpleName());
+        gameInfos.add(gameInfo);
+        gameInfo = new GameInfo(context.getString(R.string.gamelist_plane), context.getString(R.string.gamelist_plane_content), 0, null, PlaneMainActivity.class.getSimpleName());
+        gameInfos.add(gameInfo);
+        gameInfo = new GameInfo(context.getString(R.string.game_h5_zhaoyaojing), context.getString(R.string.game_h5_zhaoyaojing_content), 1, "http://h.4399.com/play/161856.htm", null);
+        gameInfo.setShare_title(context.getString(R.string.game_h5_zhaoyaojing_share));
+        gameInfo.setShare_content(context.getString(R.string.game_h5_zhaoyaojing_share_content));
+        gameInfos.add(gameInfo);
+        gameInfo = new GameInfo(context.getString(R.string.game_h5_zaqiche), context.getString(R.string.game_h5_zaqiche_content), 1, "http://h.4399.com/play/159875.htm", null);
+        gameInfo.setShare_title(context.getString(R.string.game_h5_zaqiche_share));
+        gameInfo.setShare_content(context.getString(R.string.game_h5_zaqiche_share_content));
+        gameInfos.add(gameInfo);
+        gameInfo = new GameInfo(context.getString(R.string.game_h5_wucaiguodong), context.getString(R.string.game_h5_wucaiguodong_content), 2, "http://h.4399.com/play/191530.htm", null);
+        gameInfo.setShare_title(context.getString(R.string.game_h5_wucaiguodong_share));
+        gameInfo.setShare_content(context.getString(R.string.game_h5_wucaiguodong_share_content));
+        gameInfos.add(gameInfo);
+        gameInfo = new GameInfo(context.getString(R.string.game_h5_guoshanche), context.getString(R.string.game_h5_guoshanche_content), 2, "http://h.4399.com/play/158682.htm", null);
+        gameInfo.setShare_title(context.getString(R.string.game_h5_guoshanche_share));
+        gameInfo.setShare_content(context.getString(R.string.game_h5_guoshanche_share_content));
+        gameInfos.add(gameInfo);
         return gameInfos;
     }
 }
