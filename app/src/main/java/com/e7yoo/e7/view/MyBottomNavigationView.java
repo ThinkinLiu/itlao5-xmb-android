4021fb98843835192ca01ad19625a89b01b92524
Thinkin <thinkin>
2018-04-15 22:35:33 +0800

    移除无用的view

---
+++ app/src/main/java/com/e7yoo/e7/view/MyBottomNavigationView.java
@@ -0,0 +1,23 @@
+package com.e7yoo.e7.view;
+
+import android.content.Context;
+import android.support.design.widget.BottomNavigationView;
+import android.util.AttributeSet;
+
+/**
+ * Created by andy on 2018/4/14.
+ */
+
+public class MyBottomNavigationView extends BottomNavigationView {
+    public MyBottomNavigationView(Context context) {
+        super(context);
+    }
+
+    public MyBottomNavigationView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyBottomNavigationView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
